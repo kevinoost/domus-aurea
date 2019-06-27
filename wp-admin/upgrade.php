5a815407d42c781d15b76c3dfff352e912431f99
Kevin Oost <kevin.oost@systemsnavigator.com>
2019-06-27 15:35:55 +0200

    initial generated website

---
+++ wp-admin/upgrade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+<head>
+	<meta name="viewport" content="width=device-width" />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="robots" content="noindex,nofollow" />
+	<title>WordPress &rsaquo; Update</title>
+	<link rel='stylesheet' id='buttons-css'  href='https://kevinoost.github.io/domus-aurea/wp-includes/css/buttons.min.css?ver=5.2.2' type='text/css' media='all' />
+<link rel='stylesheet' id='install-css'  href='https://kevinoost.github.io/domus-aurea/wp-admin/css/install.min.css?ver=5.2.2' type='text/css' media='all' />
+<!--[if lte IE 7]>
+<link rel='stylesheet' id='ie-css'  href='https://kevinoost.github.io/domus-aurea/wp-admin/css/ie.min.css?ver=5.2.2' type='text/css' media='all' />
+<![endif]-->
+</head>
+<body class="wp-core-ui">
+<p id="logo"><a href="https://wordpress.org/">WordPress</a></p>
+
+
+<h1>No Update Required</h1>
+<p>Your WordPress database is already up-to-date!</p>
+<p class="step"><a class="button button-large" href="https://kevinoost.github.io/domus-aurea/">Continue</a></p>
+
+	</body>
+</html>
