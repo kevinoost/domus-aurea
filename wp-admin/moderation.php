901530ad4ec3437b569e7999537aef556c8086a8
Kevin Oost <kevin.oost@systemsnavigator.com>
2019-06-27 15:48:02 +0200

    second

---
+++ wp-admin/moderation.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://kevinoost.github.io/domus-aurea/wp-admin/edit-comments.php?comment_status=moderated">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://kevinoost.github.io/domus-aurea/wp-admin/edit-comments.php?comment_status=moderated";
+		</script>
+
+		<p>You are being redirected to <a href="https://kevinoost.github.io/domus-aurea/wp-admin/edit-comments.php?comment_status=moderated">https://kevinoost.github.io/domus-aurea/wp-admin/edit-comments.php?comment_status=moderated</a></p>
+	</body>
+</html>
---
+++ wp-links-opml.php
@@ -0,0 +1,11 @@
+<?xml version="1.0"?>
+<opml version="1.0">
+	<head>
+		<title>
+		Links for Design Center 136		</title>
+		<dateCreated>Thu, 27 Jun 2019 13:43:38 GMT</dateCreated>
+		<!-- generator="WordPress/5.2.2" -->
+	</head>
+	<body>
+</body>
+</opml>
